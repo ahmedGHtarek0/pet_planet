7a0928f02b4938e9c00901f4d22a5ea9643ed9ea
ahmed tarek <ahmed01tareq10@gmail.com>
2026-08-26 18:49:33 +0300

    scaffolding backend

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config()
+const app = express()
+const port=3001
+app.use(express.json())
+
+try{
+mongoose.connect(process.env.mongodblocalhostlink??'')
+.then(()=>console.log('the youtube database connetced'))
+.catch((err)=>console.log('there error in db '+err))
+}catch(err){
+console.log('erro in connected with Db  '+err)
+}
+
+app.listen(port,()=>{
+    console.log(`the serevr is connect to the localhost http://loaclhost:${port}`)
+})
